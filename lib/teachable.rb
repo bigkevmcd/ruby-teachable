4e8ac5bfed8c553521bdc652314fbadcb9b2f186
Kevin McDermott <bigkevmcd@gmail.com>
2020-05-02 17:01:29 +0100

    Make the code slightly closer to the Smalltalk version.

--- lib/teachable.rb
+++ lib/teachable.rb
@@ -1,27 +1,28 @@
 class Teachable
-  def initialize
-    @methods = {}
-  end
-
   def when_send_then_return(method, ret)
-    @methods[method] = ret
+    learnings[method] = ret
     self
   end
 
   def when_send_then_evaluate(method, &block)
-    @methods[method] = block
+    learnings[method] = block
     self
   end
 
   def accept_send(method)
-    @methods[method] = self
+    learnings[method] = self
   end
 
   private
 
+  def learnings
+    @learnings = {} unless defined? @learnings
+    @learnings
+  end
+
   def method_missing(m, *args, &block)
-    return super unless @methods.key?(m.to_sym)
-    ret = @methods[m.to_sym]
+    return super unless learnings.key?(m.to_sym)
+    ret = learnings[m.to_sym]
     return ret.call(*args) if ret.is_a?(Proc)
     ret
   end
